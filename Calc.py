409d4e59310a7647f81e0f06cd1be282de846f14
shiv1152003 <shivamparmar1ohvy@gmail.com>
2025-06-15 12:46:59 +0530

    Add Initial file

---
+++ Calc.py
@@ -0,0 +1,13 @@
+a = int(input("Enter a Number : "))
+b = str(input("Enter An Operator : "))
+c = int(input("Enter a Number : "))
+
+if b == '+':
+    print(a,"+",c,"=",a+c)
+elif b == '-':
+    print(a,"-",c,"=",a-c)
+elif b == '*':
+    print(a,"*",c,"=",a*c)
+elif b == '/':
+    print(a,"/",c,"=",a/c)
+    
